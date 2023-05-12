2ecfd0fc8fbb85c227d495261ebe4ecbce00058b
Anton <bendzaminpirs@gmail.com>
2023-05-12 21:40:22 +0300

    программа для проверки слова на палиндром

--- lesson_3/lesson_3.py
+++ lesson_3/lesson_3.py
@@ -1,2 +1,11 @@
-input_message = input("Введите строку: ")
-print(input_message[::-1])
+# input_message = input("Введите строку: ")
+# print(input_message[::-1])
+
+user_input = input("Для проверки палиндрома введите слово: ")
+user_input = user_input.lower()
+palindrome = user_input[::-1]
+
+if user_input == palindrome:
+    print(f"Слово {user_input} является палиндромом — {palindrome}.")
+else:
+    print(f"Слово {user_input} не является палиндромом.")
