daac8c248ad8caf2fca2a9fceb93f7f364d215fb
Anton <bendzaminpirs@gmail.com>
2023-05-28 20:22:59 +0300

    Добавил цикл, диалог.

--- homework_lesson_6/homework_lesson_6_1.py
+++ homework_lesson_6/homework_lesson_6_1.py
@@ -1,25 +1,51 @@
 # Для использования .punctuation
 import string
 
-user_input = input("Для проверки палиндрома введите текст: ")
-user_input = user_input.lower()
-# Убираем переносы и табуляцию по краям.
-user_input = user_input.strip()
-# Убираем переносы и табуляцию внутри строки.
-user_input = user_input.replace("\n", "")
-user_input = user_input.replace("\t", "")
-# Убираем пробелы внутри предложения.
-# Иначе не заметит ввод по типу "Аби ріці риба".
-user_input = user_input.replace(" ", "")
+# Добавил всё в цикл, чтобы имитировать какое-никакое общение.
+while True:
+    initial_input = input("Для запуска введите «Старт», "
+                          "для выхода введите «Выход»."
+                          "\n> ")
+    initial_input = initial_input.lower()
+    initial_input = initial_input.strip()
 
-# Убираем все знаки препинания с помощью цикла.
-for character in string.punctuation:
-    user_input = user_input.replace(character, "")
-# Создаем переменную, чтобы затем использовать её в if/else.
-palindrome = user_input[::-1]
+    if initial_input == "старт":
+        while True:
 
-# Собственно, сама проверка на палиндром.
-if user_input == palindrome:
-    print(f"Введённый текст является палиндромом — {palindrome}.")
-else:
-    print(f"{user_input} не является палиндромом.")
+            user_input = input("Для проверки палиндрома введите текст:"
+                               "\n> ")
+            user_input = user_input.lower()
+            # Убираем переносы и табуляцию по краям.
+            user_input = user_input.strip()
+            # Убираем переносы и табуляцию внутри строки.
+            user_input = user_input.replace("\n", "")
+            user_input = user_input.replace("\t", "")
+            # Убираем пробелы внутри предложения.
+            # Иначе не заметит ввод по типу "Аби ріці риба".
+            user_input = user_input.replace(" ", "")
+
+            # Убираем все знаки препинания с помощью цикла.
+            for character in string.punctuation:
+                user_input = user_input.replace(character, "")
+
+            # Создаем переменную, чтобы затем использовать её в if/else.
+            palindrome = user_input[::-1]
+
+            # Собственно, сама проверка на палиндром.
+            if user_input == palindrome:
+                print(f"Введённый текст является палиндромом — {palindrome}.")
+            elif user_input == "выход":
+                break
+            # Небольшой прикол.
+            elif palindrome == "выход":
+                print("Кажется, вы нашли пасхалочку.")
+                break
+            else:
+                print(f"{user_input} не является палиндромом.")
+    elif initial_input == "выход":
+        print("Выход из программы..."
+              "\n...завершён.")
+        quit()
+    else:
+        print("Некорректный ввод. Попробуйте заново.")
+        continue
