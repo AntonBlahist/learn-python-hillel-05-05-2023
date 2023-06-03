675460e9238f785156093c014f8acb5f97395dfe
Anton <bendzaminpirs@gmail.com>
2023-06-03 22:33:37 +0300

    Готово

--- homework_lesson_7/homework_lesson_7_2.py
+++ homework_lesson_7/homework_lesson_7_2.py
@@ -3,31 +3,66 @@ import math
 
 def triangle_side():
     """
-    Считывание стороны треугольника (положительного числа),
-    возвращение этого значения.
+    Creating a list of triangle sides.
+    Return <triangle sides>.
     """
-    pass
+    triangle_sides = []
+    a = int(input("Enter side A: "))
+    triangle_sides.append(a)
+    b = int(input("Enter side B: "))
+    triangle_sides.append(b)
+    c = int(input("Enter side C: "))
+    triangle_sides.append(c)
+    for x in triangle_sides:
+        if x <= 0:
+            print("The side of the triangle must be greater than 0.")
+            exit()
+    return triangle_sides
 
 
-def triangle_exist():
+def triangle_exist(sides):
     """
-    Проверка существования треугольника,
-    возвращение True/False.
+    Verifying the existence of a triangle.
+    Return True or False.
     """
-    pass
+    if (sides[0] + sides[1]) > sides[2] \
+            and (sides[0] + sides[2]) > sides[1] \
+            and (sides[1] + sides[2]) > sides[0]:
+        return True
+    else:
+        return False
 
 
-def triangle_perimeter():
+def triangle_perimeter(sides):
     """
-    Расчёт периметра треугольника,
-    возвращение периметра.
+    Calculating the perimeter of a triangle.
+    Return the perimeter.
     """
-    pass
+    func_perimeter = sum(sides)
+    return func_perimeter
 
 
-def triangle_area():
+def triangle_area(sides):
     """
-    Расчёт площади треугольника,
-    возвращение площади.
+    Calculating the area of a triangle.
+    Return the area.
     """
-    pass
+    half_p = sum(sides) / 2
+    func_area = math.sqrt(half_p
+                          * (half_p - sides[0])
+                          * (half_p - sides[1])
+                          * (half_p - sides[2]))
+    return func_area
+
+
+if __name__ == "__main__":
+    print("The program calculates the perimeter and area of a triangle.")
+    side = triangle_side()
+    exist = triangle_exist(side)
+    if exist:
+        perimeter = triangle_perimeter(side)
+        area = triangle_area(side)
+        print(f"A triangle exists. Its perimeter is {perimeter},"
+              f" its area is {area}.")
+    if not exist:
+        print("A triangle with the sides indicated does not exist.")
