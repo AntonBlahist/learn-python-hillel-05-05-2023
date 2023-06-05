31ab78de0f704794ed151ae6e04149691e982d13
Anton <bendzaminpirs@gmail.com>
2023-06-05 22:15:38 +0300

    fixed calculation and automatized dict. add comments

--- homework_lesson_8/homework_lesson_8_1.py
+++ homework_lesson_8/homework_lesson_8_1.py
@@ -1,20 +1,48 @@
-converting_dict = {"days": 0, "hours": 0, "minutes": 0, "seconds": 0}
-
-# if __name__ == "__main__":
-# pass
-
+#  Decided to use constants.
 HOUR_IN_DAY = 24
-MIN_IN_DAY = 1440
-SEC_IN_DAY = 86400
 MIN_IN_HOUR = 60
-SEC_IN_HOUR = 3600
 SEC_IN_MIN = 60
 
-sec = int(input("Enter value in seconds: "))
 
-days = divmod(sec, SEC_IN_DAY)
-hours = divmod(sec, SEC_IN_HOUR)
-minutes = divmod(sec, SEC_IN_MIN)
-seconds = minutes[1]
-print(days, hours, minutes, seconds)
-print(converting_dict)
+def converting(seconds):
+    """
+    The function checks if the input is a negative number or a string.
+    If it is, the program is closed.
+    If the input is correct, the function converts seconds
+    into days, hours, minutes and seconds.
+    It returns the tuple of the corresponding variables.
+    """
+    try:
+        seconds = int(seconds)
+        if not seconds <= 0:
+            minutes, seconds = divmod(seconds, SEC_IN_MIN)
+            hours, minutes = divmod(minutes, MIN_IN_HOUR)
+            days, hours = divmod(hours, HOUR_IN_DAY)
+            result_tuple = (days, hours, minutes, seconds)
+            return result_tuple
+        else:
+            print("Oops! Time cannot be negative.")
+            exit()
+    except ValueError:
+        print("Please, enter a number only.")
+        exit()
+
+
+def converting_dict(result_tuple):
+    """
+    The function creates a dictionary with the values from <result_tuple>.
+    It returns the dictionary with the corresponding values.
+    """
+    #  The dictionary contains results as values.
+    dictionary = {"days": result_tuple[0],
+                  "hours": result_tuple[1],
+                  "minutes": result_tuple[2],
+                  "seconds": result_tuple[3]}
+    return dictionary
+
+
+if __name__ == "__main__":
+    sec = input("Enter a value in seconds: ")
+    result = converting(sec)
+    result_dict = converting_dict(result)
+    print(result_dict)
