c2ac9aa0c4dd25ced119cfaf35eec883d6989725
Anton <bendzaminpirs@gmail.com>
2023-06-10 18:17:43 +0300

    skeleton

---
+++ homework_lesson_10/homework_lesson_10.py
@@ -0,0 +1,44 @@
+def notes_add():
+    """
+    The function saves user's input into (list, tuple, dict, set),
+    saves the input length (preferably use tuple or dict),
+    and returns the corresponding (list, tuple, dict, set).
+    """
+
+
+def notes_all():
+    """
+    The function returns all the saved notes.
+    """
+
+
+def notes_earliest():
+    """
+    The function returns saved notes in chronological order -
+    from the earliest to the latest.
+    """
+
+
+def notes_latest():
+    """
+    The function returns the saved notes in chronological order -
+    from the latest to the earliest.
+    """
+
+
+def notes_longest():
+    """
+    The function returns the saved notes in order of their length -
+    from the longest to the shortest.
+    """
+
+
+def notes_shortest():
+    """
+    The function returns the saved notes in order of their length -
+    from the shortest to the longest.
+    """
+
+
+if __name__ == "__main__":
+    pass
