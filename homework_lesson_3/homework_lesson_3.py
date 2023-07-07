7de2ab602144df71664e301288e15264c8ceff55
Anton <bendzaminpirs@gmail.com>
2023-07-08 02:48:38 +0300

    Fix a bit

--- homework_lesson_3/homework_lesson_3.py
+++ homework_lesson_3/homework_lesson_3.py
@@ -1,42 +1,31 @@
-# Задал переменную для предложения пользователя.
-user_input_string = input('Введите предложение: ')
-
-# .strip с символами не работал внутри строки.
-# Применил replace к каждому символу.
-# .lower для нижнего регистра.
-# .rstrip для удаления пробелов справа.
-
-user_input_string = user_input_string.lower()
-user_input_string = user_input_string.replace('.', '')
-user_input_string = user_input_string.replace(',', '')
-user_input_string = user_input_string.replace('-', '')
-user_input_string = user_input_string.replace(':', '')
-user_input_string = user_input_string.replace(';', '')
-user_input_string = user_input_string.replace('?', '')
-user_input_string = user_input_string.replace('!', '')
-user_input_string = user_input_string.rstrip()
-
-
-def find_replace():
+def find_replace(user_text: str):
     """
     string_to_replace для ввода слова, которое будем менять,
     index_to_replace для метода .find, поиска и вывода индекса,
     а также для блока if/else.
     """
-    string_to_replace = input('Введите слово или словосочетание, '
-                              'которое вы хотите заменить в предложении: ')
-    index_to_replace = user_input_string.find(string_to_replace)
+    user_text = user_text.lower()
+    punctuation = [".", ",", "-", ":", ";", "?", "!"]
+    for symbol in punctuation:
+        user_text = user_text.replace(symbol, "")
+    user_text = user_text.rstrip()
+
+    string_to_replace = input("Введите слово или словосочетание, "
+                              "которое вы хотите заменить в предложении: ")
+    index_to_replace = user_text.find(string_to_replace)
+
     if index_to_replace == -1:
-        print(f'"{string_to_replace}" не найдено в предложении.')
-        find_replace()
+        print(f"{string_to_replace} не найдено в предложении.")
+        find_replace(user_text)
     else:
-        print(f'"{string_to_replace}" найдено. Индекс = {index_to_replace}.')
-        new_string = input('Введите слово или словосочетание, '
-                           'на которое вы хотите заменить: ')
-        updated_string = user_input_string.replace(string_to_replace, new_string)
-        print('Результат: ')
+        print(f"{string_to_replace} найдено. Индекс = {index_to_replace}.")
+        new_string = input("Введите слово или словосочетание, "
+                           "на которое вы хотите заменить: ")
+        updated_string = user_text.replace(string_to_replace, new_string)
+        print("Результат: ")
         print(updated_string)
 
 
 if __name__ == "__main__":
-    find_replace()
+    user_input_string = input("Введите предложение: ")
+    find_replace(user_input_string)
