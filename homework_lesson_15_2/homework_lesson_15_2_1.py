e69cde8f0aef66d2b35c3e9f6e37b1a7ba26f22a
Anton <bendzaminpirs@gmail.com>
2023-07-10 06:48:22 +0300

    Add a hint for myself.

--- homework_lesson_15_2/homework_lesson_15_2_1.py
+++ homework_lesson_15_2/homework_lesson_15_2_1.py
@@ -3,3 +3,8 @@ from dataprocessor import DataEntry, MetricCalculator, FileProcessor
 
 if __name__ == "__main__":
     pass
+
+"""
+Из папки с файлами создаю список этих файлов с помощью "from os import listdir", например,
+и уже к каждому файлу из списка применяю чтение.
+"""
